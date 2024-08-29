dd789febda801581d01d734aaebde3d0e6f5f6ee
Evgeniy Valyaev <e.valiaev@indriver.com>
2024-08-30 01:41:20 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://gitname.github.io/auth-form",
+  base: "https://valyay.github.io/auth-form",
   plugins: [react()],
 });
